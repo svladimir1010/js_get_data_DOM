486bb924f2f42d5aa0b84c8958fb0a2e4008442a
svladimir1010 <vladimireur@gmail.com>
2026-09-17 15:07:28 +0300

    feat: parse population data and calculate totals

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('.population')];
+
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+let totalPopulation = 0;
+
+population.forEach((i) => {
+  const number = parseInt(i.innerText.replaceAll(',', ''), 10);
+
+  totalPopulation += number;
+});
+
+const averagePopulation = Math.round(
+  totalPopulation / population.length,
+).toLocaleString('en-US');
+
+const formattedTotal = totalPopulation.toLocaleString('en-US');
+
+total.innerText = formattedTotal;
+average.innerText = averagePopulation;
